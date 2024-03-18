38af0cdce3d74c5b6c98cc3acafbec28b0e144da
Brian <thugmba@gmail.com>
2024-03-18 17:53:58 +0800

    .

--- W5_1_Barchart.R
+++ W5_1_Barchart.R
@@ -2,4 +2,9 @@
 # Read csv file and transform
 # Draw bar chart
 
+library(dplyr)
+
 accidents <- read.csv('Accidents.csv')
+accidents %>% pivot_longer(cols=c('Death','Injured'),
+                           names_to ='Type',
+                           values_to ='Count')
